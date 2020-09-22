ab60889e398150ced43296342a70f10f9292b7f9
Julian Popescu <jpopesculian@gmail.com>
2020-09-22 15:56:18 +0200

    nloq no_std compatibility

--- nloq/src/lib.rs
+++ nloq/src/lib.rs
@@ -1,6 +1,11 @@
 //! Parses natural language to produce a `Vec<Query>`. The main entrypoints are
 //! `from_slice` and `from_str`
+#![cfg_attr(not(feature = "std"), no_std)]
 
+#[cfg(feature = "std")]
+extern crate std as core;
+
+extern crate alloc;
 pub extern crate object_query;
 
 mod de;
